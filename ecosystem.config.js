372be1afb76f393c794fa52ba1f2da07346275f5
lslove10010 <96574438+lslove10010@users.noreply.github.com>
2024-09-07 21:25:10 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,5 +2,9 @@ module.exports = {
     apps: [{
         name: "uptime-kuma",
         script: "./server/server.js",
+        env: {
+            PORT: "port",
+            PLAYWRIGHT_BROWSERS_PATH: "/nonexistent"
+        }
     }]
 };
